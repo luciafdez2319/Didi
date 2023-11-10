7a5d1d6bb1a208f7977b2968c426f7d7ddb58fcc
Lucía Fernández <57848577+luciafdez2319@users.noreply.github.com>
2023-11-09 18:05:56 -0600

    Update LuciaFernandez_c2.sql

--- LuciaFernandez_c2.sql
+++ LuciaFernandez_c2.sql
@@ -6,6 +6,6 @@ restaurants.
 SELECT  di.day_of_week, AVG(rv.reserve_visitors) AS avg_visitors
 FROM restaurants_visitors rv, date_info di 
 WHERE di.calendar_date = rv.visit_date
-GROUP BY di.day_of_week
+GROUP BY 1
 ORDER BY avg_visitors DESC 
 LIMIT 1;
